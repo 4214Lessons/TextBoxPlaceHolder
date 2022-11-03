1a6625e740bae6dabc499e0604d08e1d0ff4f316
Tural Novruzov <t.novruzov@itstep.edu.az>
2022-11-03 16:30:17 +0400

    Add project files.

---
+++ Source/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Source;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void TextBoxPlaceHolderUC_TextChangedUC(object sender, TextChangedEventArgs e)
+    {
+        var control = sender as TextBoxPlaceHolderUC;
+
+        MessageBox.Show(control?.Text);
+    }
+}
---
+++ Source/TextBoxPlaceHolderUC.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Source;
+
+public partial class TextBoxPlaceHolderUC : UserControl
+{
+    public TextBoxPlaceHolderUC()
+    {
+        InitializeComponent();
+    }
+
+
+    public string PlaceHolderText
+    {
+        get { return (string)GetValue(PlaceHolderTextProperty); }
+        set { SetValue(PlaceHolderTextProperty, value); }
+    }
+
+    public static readonly DependencyProperty PlaceHolderTextProperty =
+        DependencyProperty.Register("PlaceHolderText", typeof(string), typeof(TextBoxPlaceHolderUC));
+
+
+
+
+    public string Text
+    {
+        get { return (string)GetValue(TextProperty); }
+        set { SetValue(TextProperty, value); }
+    }
+
+    public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register("Text", typeof(string), typeof(TextBoxPlaceHolderUC));
+
+
+
+
+
+    public bool AcceptsReturn
+    {
+        get { return (bool)GetValue(AcceptsReturnProperty); }
+        set { SetValue(AcceptsReturnProperty, value); }
+    }
+
+    public static readonly DependencyProperty AcceptsReturnProperty =
+        DependencyProperty.Register("AcceptsReturn", typeof(bool), typeof(TextBoxPlaceHolderUC));
+
+
+
+    public event TextChangedEventHandler TextChangedUC;
+
+    private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        TextChangedUC?.Invoke(this, e);
+    }
+}
